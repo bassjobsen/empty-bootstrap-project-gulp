6eca80682e8bb7ef06a314af32845a93b10edc7a
Bass Jobsen <bass@w3masters.nl>
2016-04-13 22:58:56 +0200

    first version

---
+++ Gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var mq4HoverShim = require('mq4-hover-shim');
+var rimraf = require('rimraf').sync;
+var browser = require('browser-sync');
+var panini = require('panini');
+var concat = require('gulp-concat');
+var port = process.env.SERVER_PORT || 8080;
+var bowerpath = process.env.BOWER_PATH || 'bower_components/';
+
+// Starts a BrowerSync instance
+gulp.task('server', ['build'], function(){
+  browser.init({server: './_site', port: port});
+});
+
+// Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*', ['compile-sass', browser.reload]);
+  gulp.watch('html/**/*.html', ['compile-html', browser.reload]);
+});
+
+// Erases the dist folder
+gulp.task('clean', function() {
+  rimraf('_site');
+});
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded',
+  includePaths: bowerpath
+};
+
+gulp.task('compile-sass', function () {
+    var processors = [
+        mq4HoverShim.postprocessorFor({ hoverSelectorPrefix: '.bs-true-hover ' }),
+        autoprefixer({
+            browsers: [
+              //
+              // Official browser support policy:
+              // http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
+              //
+              'Chrome >= 35', // Exact version number here is kinda arbitrary
+              // Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
+              // we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
+              // (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
+              //     such that folks haven't yet had a reasonable amount of time to upgrade; and
+              // (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
+              //     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
+              //     Since they've been unprefixed, Autoprefixer will stop prefixing them,
+              //     thus causing them to not work in the previous ESR (where the prefixes were required).
+              'Firefox >= 31', // Current Firefox Extended Support Release (ESR)
+              // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
+              // NOT the Edge app version shown in Edge's "About" screen.
+              // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
+              // See also https://github.com/Fyrd/caniuse/issues/1928
+              'Edge >= 12',
+              'Explorer >= 9',
+              // Out of leniency, we prefix these 1 version further back than the official policy.
+              'iOS >= 8',
+              'Safari >= 8',
+              // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
+              'Android 2.3',
+              'Android >= 4',
+              'Opera >= 12'
+            ]
+          })//,
+        //cssnano(),
+    ];
+    return gulp.src('./scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./_site/css/'));
+});
+
+gulp.task('compile-html', function() {
+  gulp.src('html/pages/**/*.html')
+    .pipe(panini({
+      root: 'html/pages/',
+      layouts: 'html/layouts/',
+      partials: 'html/includes/',
+      helpers: 'html/helpers/',
+      data: 'html/data/'
+    }))
+    .pipe(gulp.dest('_site'));
+});
+
+gulp.task('compile-js', function() {
+  return gulp.src([bowerpath+ 'jquery/dist/jquery.min.js', bowerpath+ 'tether/dist/js/tether.min.js', bowerpath+ 'bootstrap/dist/js/bootstrap.min.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./_site/js/'));
+});
+
+
+gulp.task('build', ['clean','compile-js','compile-sass','compile-html']);
+gulp.task('default', ['server', 'watch']);
---
+++ README.md
@@ -0,0 +1,48 @@
+# Empty Boostrap 4 Project
+
+Empty [Bootstrap 4](http://v4-alpha.getbootstrap.com/) Project. It includes a Sass compiler and a set of Panini HTML templates for you. [Panini](https://github.com/zurb/panini) is a super simple flat file generator for use with Gulp. It compiles a series of HTML pages using a common layout. These pages can also include HTML partials, external Handlebars helpers, or external data as JSON or YAML.
+
+## Installation
+
+To use this template, your computer needs:
+
+- [NodeJS](https://nodejs.org/en/) (0.12 or greater)
+- [Git](https://git-scm.com/)
+
+This template can be installed with the Bootstrap CLI, or downloaded and set up manually.
+
+### Using the CLI
+
+Install the Bootstrap CLI with this command:
+
+```bash
+[sudo] npm install -g gulp bower
+npm install bootstrap-cli --global
+```
+
+Use this command to set up an empty Bootstrap 4 project:
+
+```bash
+bootstrap new --template empty-bootstrap-project-gulp
+```
+
+The CLI will prompt you to give your project a name. The code will be downloaded into a folder with this name.
+
+### Manual Setup
+
+To manually set up the project, first download it with Git:
+
+```bash
+git clone https://github.com/bassjobsen/empty-bootstrap-project-gulp projectname
+```
+
+Then open the folder in your command line, and install the needed dependencies:
+
+```bash
+cd projectname
+npm install
+bower install
+```
+
+Finally, run `npm start` to run the Sass and HTML template compiler. They will re-run every time you save a Sass or HTML temaplate file.
+
