6a74486b7632addcc19a8705ad224a7445a09bdb
Bass Jobsen <bass@w3masters.nl>
2017-12-05 00:23:04 +0100

    upgrade to Bootstrap 4.0.0-beta.2

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,7 +10,7 @@ var browser = require('browser-sync');
 var panini = require('panini');
 var concat = require('gulp-concat');
 var port = process.env.SERVER_PORT || 8080;
-var bowerpath = process.env.BOWER_PATH || 'bower_components/';
+var nodepath =  'node_modules/';
 
 // Starts a BrowerSync instance
 gulp.task('server', ['build'], function(){
@@ -37,7 +37,7 @@ gulp.task('copy', function() {
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'expanded',
-  includePaths: bowerpath
+  includePaths: [nodepath + 'bootstrap/scss/']
 };
 
 gulp.task('compile-sass', function () {
@@ -45,33 +45,14 @@ gulp.task('compile-sass', function () {
         mq4HoverShim.postprocessorFor({ hoverSelectorPrefix: '.bs-true-hover ' }),
         autoprefixer({
             browsers: [
-              //
-              // Official browser support policy:
-              // http://v4-alpha.getbootstrap.com/getting-started/browsers-devices/#supported-browsers
-              //
-              'Chrome >= 35', // Exact version number here is kinda arbitrary
-              // Rather than using Autoprefixer's native "Firefox ESR" version specifier string,
-              // we deliberately hardcode the number. This is to avoid unwittingly severely breaking the previous ESR in the event that:
-              // (a) we happen to ship a new Bootstrap release soon after the release of a new ESR,
-              //     such that folks haven't yet had a reasonable amount of time to upgrade; and
-              // (b) the new ESR has unprefixed CSS properties/values whose absence would severely break webpages
-              //     (e.g. `box-sizing`, as opposed to `background: linear-gradient(...)`).
-              //     Since they've been unprefixed, Autoprefixer will stop prefixing them,
-              //     thus causing them to not work in the previous ESR (where the prefixes were required).
-              'Firefox >= 31', // Current Firefox Extended Support Release (ESR)
-              // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
-              // NOT the Edge app version shown in Edge's "About" screen.
-              // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
-              // See also https://github.com/Fyrd/caniuse/issues/1928
-              'Edge >= 12',
-              'Explorer >= 9',
-              // Out of leniency, we prefix these 1 version further back than the official policy.
-              'iOS >= 8',
-              'Safari >= 8',
-              // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
-              'Android 2.3',
-              'Android >= 4',
-              'Opera >= 12'
+                "Chrome >= 45",
+                "Firefox ESR",
+                "Edge >= 12",
+                "Explorer >= 10",
+                "iOS >= 9",
+                "Safari >= 9",
+                "Android >= 4.4",
+                "Opera >= 30"
             ]
           })//,
         //cssnano(),
@@ -103,7 +84,7 @@ gulp.task('compile-html:reset', function(done) {
 });
 
 gulp.task('compile-js', function() {
-  return gulp.src([bowerpath+ 'jquery/dist/jquery.min.js', bowerpath+ 'tether/dist/js/tether.min.js', bowerpath+ 'bootstrap/dist/js/bootstrap.min.js'])
+  return gulp.src([ nodepath + 'jquery/dist/jquery.min.js', nodepath + 'popper.js//dist/umd/popper.min.js', nodepath + 'bootstrap/dist/js/bootstrap.min.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./_site/js/'));
 });
